8bf733de71ee00bd833b0a37f1a01ed92a8ffe58
Michelle Gower <mgower@illinois.edu>
2015-10-22 19:09:14 +0000

    Changed to use new config.getfull function Misc cleanup

--- libexec/logpost.py
+++ libexec/logpost.py
@@ -4,18 +4,19 @@
 # $LastChangedBy::                        $:  # Author of last commit.
 # $LastChangedDate::                      $:  # Date of last commit.
 
+""" Bookkeeping steps executed submit-side after certain submit-side tasks """
+
 import sys
 import os
 
 import processingfw.pfwdefs as pfwdefs
 import despymisc.miscutils as miscutils
 import processingfw.pfwconfig as pfwconfig
-import processingfw.pfwdb as pfwdb
 from processingfw.pfwlog import log_pfw_event
-from processingfw.pfwemail import send_subblock_email
 
 
-def logpost(argv = None):
+def logpost(argv=None):
+    """ Program entry point """
     if argv is None:
         argv = sys.argv
 
@@ -23,13 +24,13 @@ def logpost(argv = None):
     debugfh = open('logpost.out', 'w', 0)
     sys.stdout = debugfh
     sys.stderr = debugfh
-    
+
     print ' '.join(argv)  # print command line for debugging
-    
+
     if len(argv) < 5:
         print 'Usage: logpost configfile block subblocktype subblock retval'
         debugfh.close()
-        return(pfwdefs.PF_EXIT_FAILURE)
+        return pfwdefs.PF_EXIT_FAILURE
 
     configfile = argv[1]
     blockname = argv[2]
@@ -49,49 +50,46 @@ def logpost(argv = None):
     config = pfwconfig.PfwConfig({'wclfile': configfile})
     if miscutils.fwdebug_check(3, 'PFWPOST_DEBUG'):
         miscutils.fwdebug_print("done reading config file")
-    if miscutils.convertBool(config[pfwdefs.PF_USE_DB_OUT]): 
-        dbh = pfwdb.PFWDB(config['submit_des_services'], config['submit_des_db_section'])
-        #dbh.update_blktask_end(config, "", subblock, retval)
 
     # now that have more information, rename output file
     if miscutils.fwdebug_check(3, 'PFWPOST_DEBUG'):
         miscutils.fwdebug_print("before get_filename")
-    blockname = config['blockname']
-    blkdir = config['block_dir']
-    new_log_name = config.get_filename('block', {pfwdefs.PF_CURRVALS: 
-                                                  {'flabel': '${subblock}_logpost', 
-                                                   'subblock': subblock,
-                                                   'fsuffix':'out'}})
+    blockname = config.getfull('blockname')
+    blkdir = config.getfull('block_dir')
+    new_log_name = config.get_filename('block',
+                                       {pfwdefs.PF_CURRVALS: {'flabel': '${subblock}_logpost',
+                                                              'subblock': subblock,
+                                                              'fsuffix':'out'}})
     new_log_name = "%s/%s" % (blkdir, new_log_name)
     miscutils.fwdebug_print("new_log_name = %s" % new_log_name)
-     
+
     debugfh.close()
     os.chmod('logpost.out', 0666)
     os.rename('logpost.out', new_log_name)
     debugfh = open(new_log_name, 'a+')
     sys.stdout = debugfh
     sys.stderr = debugfh
-    
-    
+
+
     log_pfw_event(config, blockname, subblock, subblocktype, ['posttask', retval])
-    
+
     # In order to continue, make pipelines dagman jobs exit with success status
     #if 'pipelinesmngr' not in subblock:
     #    retval = pfwdefs.PF_EXIT_SUCCESS
-    
+
 #    # If error at non-manager level, send failure email
 #    if retval != pfwdefs.PF_EXIT_SUCCESS and \
 #        'mngr' not in subblock:
 #        send_subblock_email(config, blockname, subblock, retval)
-    
+
     if retval != pfwdefs.PF_EXIT_SUCCESS:
         miscutils.fwdebug_print("Setting failure retval")
-        retval = pfwdefs.PF_EXIT_FAILURE     
-    
+        retval = pfwdefs.PF_EXIT_FAILURE
+
     miscutils.fwdebug_print("returning retval = %s" % retval)
     miscutils.fwdebug_print("logpost done")
     debugfh.close()
-    return(int(retval))
+    return int(retval)
 
 if __name__ == "__main__":
     realstdout = sys.stdout

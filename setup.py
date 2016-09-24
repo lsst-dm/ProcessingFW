87a85e07bddeaa00ea2812b63242e4d36204231c
Gregory Daues <daues@illinois.edu>
2016-09-24 14:03:52 +0000

    prepare for next tag

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ share_files = glob.glob("share/condor/*")
 
 # The main call
 setup(name='ProcessingFW',
-      version ='2.1.3',
+      version ='2.1.4',
       license = "GPL",
       description = "DESDM's processing framework",
       author = "Michelle Gower",
